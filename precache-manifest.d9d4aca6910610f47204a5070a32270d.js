b173c5f12ad3a23595fcd8ada5a2573d73185f66
Rannie Peralta <rannieperalta092796@gmail.com>
2019-11-09 21:30:26 +0800

    Updates

---
+++ precache-manifest.d9d4aca6910610f47204a5070a32270d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a3e8206ed1388d373fbf9be11467c5d",
+    "url": "/ui-guide-website/index.html"
+  },
+  {
+    "revision": "d15905824d80d4543387",
+    "url": "/ui-guide-website/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "b82c49ee5483ef0c5629",
+    "url": "/ui-guide-website/static/js/2.ebb193a7.chunk.js"
+  },
+  {
+    "revision": "d15905824d80d4543387",
+    "url": "/ui-guide-website/static/js/main.e66a17f8.chunk.js"
+  },
+  {
+    "revision": "bf4c71acfb092535bd53",
+    "url": "/ui-guide-website/static/js/runtime-main.646ba1c6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ui-guide-website/static/media/logo.25bf045c.svg"
+  }
+]);
